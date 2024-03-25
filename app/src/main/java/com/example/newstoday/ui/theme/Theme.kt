626e4cb0dc9013574dc752fdcdb807aa17aeb7c6
lord-of-the-lost <ignatov.99@yandex.ru>
2024-03-25 12:55:36 +0300

    initial commit

--- app/src/main/java/com/example/newstoday/ui/theme/Theme.kt
+++ app/src/main/java/com/example/newstoday/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.newstoday.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,6 +10,11 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
 	primary = Purple80,
@@ -30,6 +36,27 @@ private val LightColorScheme = lightColorScheme(
 	onBackground = Color(0xFF1C1B1F),
 	onSurface = Color(0xFF1C1B1F),
 	*/
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
 )
 
 @Composable
