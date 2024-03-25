356b179b198f52391a8c6bf3116108871a89604a
lord-of-the-lost <ignatov.99@yandex.ru>
2024-03-25 12:55:36 +0300

    initial commit

---
+++ app/src/main/java/com/example/newstoday/views/SampleView.kt
@@ -0,0 +1,9 @@
+package com.example.newstoday.views
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SampleView() {
+    Text(text = "SampleView")
+}
