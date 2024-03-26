34b5333f6d8aaae5555eb40ee92eabccadb4cce9
alemivas <hskthefglab9@mail.ru>
2024-03-26 06:47:06 +0400

    Test

---
+++ app/src/main/java/com/example/newstoday/views/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.example.newstoday.views
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Text(text = "SampleView MainScreen()")
+}
