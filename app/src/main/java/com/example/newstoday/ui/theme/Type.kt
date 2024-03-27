915e6bdec4c20c00caad862fbcec8e5030080141
Razviter <p.belonogov@hotmail.com>
2024-03-27 15:40:39 +0800

    Добавил OnboardingScreen шрифты и прочее для него. Нужно подумать над анимацией в HorizontalPager.

--- app/src/main/java/com/example/newstoday/ui/theme/Type.kt
+++ app/src/main/java/com/example/newstoday/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.example.newstoday.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.newstoday.R
+
+val inter = FontFamily(
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
