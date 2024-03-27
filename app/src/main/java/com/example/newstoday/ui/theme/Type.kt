b3f9f5ef6ac039b478a3b84f11f2740a55995881
Razviter <p.belonogov@hotmail.com>
2024-03-27 17:38:32 +0800

    Добавил SplashScreen

--- app/src/main/java/com/example/newstoday/ui/theme/Type.kt
+++ app/src/main/java/com/example/newstoday/ui/theme/Type.kt
@@ -9,33 +9,38 @@ import androidx.compose.ui.unit.sp
 import com.example.newstoday.R
 
 val inter = FontFamily(
-    Font(R.font.inter_regular, FontWeight.Normal),
-    Font(R.font.inter_bold, FontWeight.Bold)
+	Font(R.font.inter_regular, FontWeight.Normal),
+	Font(R.font.inter_bold, FontWeight.Bold)
 )
+
+val adamina = FontFamily(
+	Font(R.font.adamina_regular, FontWeight.Normal)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+	bodyLarge = TextStyle(
+		fontFamily = FontFamily.Default,
+		fontWeight = FontWeight.Normal,
+		fontSize = 16.sp,
+		lineHeight = 24.sp,
+		letterSpacing = 0.5.sp
+	)
+	/* Other default text styles to override
+	titleLarge = TextStyle(
+		fontFamily = FontFamily.Default,
+		fontWeight = FontWeight.Normal,
+		fontSize = 22.sp,
+		lineHeight = 28.sp,
+		letterSpacing = 0.sp
+	),
+	labelSmall = TextStyle(
+		fontFamily = FontFamily.Default,
+		fontWeight = FontWeight.Medium,
+		fontSize = 11.sp,
+		lineHeight = 16.sp,
+		letterSpacing = 0.5.sp
+	)
+	*/
 )
