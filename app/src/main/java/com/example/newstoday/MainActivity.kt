0767b49b5f2927011d89ee1d1b42707563dfd5e7
Sam1549 <Sam1549@yandex.ru>
2024-03-30 19:44:46 +0400

    Отредактировал нижний бар

--- app/src/main/java/com/example/newstoday/MainActivity.kt
+++ app/src/main/java/com/example/newstoday/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.newstoday.navigation.MainScreen
+import com.example.newstoday.views.profiles.navigate.Navigate
 import com.example.newstoday.ui.theme.NewsToDayTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +25,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                   ProfileScreen()
+                    MainScreen()
                 }
             }
         }
