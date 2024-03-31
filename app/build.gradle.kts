26a7afdb712c7e24e4c02f21661429bcde2981db
lord-of-the-lost <ignatov.99@yandex.ru>
2024-03-31 12:11:13 +0300

    add network layer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,10 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.compose.material3:material3:1.0.0-alpha06")
-    implementation ("io.coil-kt:coil-compose:1.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha06")
+    implementation("io.coil-kt:coil-compose:1.4.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material:1.4.2")
     implementation("androidx.core:core-ktx:1.10.1")
